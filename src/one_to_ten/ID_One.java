93c86937d1f12370d53a197d58e9dc0beb678cab
Chris <chrisfender0@gmail.com>
2021-09-17 16:14:11 -0700

    first problem solved

---
+++ src/one_to_ten/ID_One.java
@@ -0,0 +1,15 @@
+package one_to_ten;
+
+public class ID_One {
+
+	public static void main(String[] args) {
+		int result = 0;
+		for(int i=1; i<1000; i++) {
+			if(i%3==0 || i%5==0) {
+				result += i;
+			}
+		}
+		System.out.println(result);
+	}
+
+}
